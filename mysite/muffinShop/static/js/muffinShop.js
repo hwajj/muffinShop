1c20826c7d178455a4d1a5e00c87db610eafaa5d
hwajj <azamwara@gmail.com>
2020-11-15 19:38:19 +0900

    Fix product select function

--- mysite/muffinShop/static/js/muffinShop.js
+++ mysite/muffinShop/static/js/muffinShop.js
@@ -2,30 +2,21 @@ var product = document.querySelectorAll('.product');
 var productNumber = document.querySelector('#id_product_number');
 var productQt = document.querySelector('#id_quantity');
 
-var productPicked;
-var quantity;
-
 for (var i = 0; i < product.length; i++) {
   product[i].addEventListener("click", clickOne);
 }
 
-function clickOne(e) { 
-    var target =  e.currentTarget;
-    var className = target.className;
-    var inputvalue = parseInt(productQt.value);
-    if (productPicked != target.id) {
-        inputvalue = 1;
-        console.log(inputvalue);
-        console.log(parseInt(target.innerHTML[0]))
-        productQt.setAttribute("value", inputvalue); 
-        productNumber.setAttribute("value", parseInt(target.innerHTML[0]));
-        productPicked = target.id;
-      
+function clickOne(e) {
+    var target =  e.target;
+    var inputNumValue = parseInt(productNumber.value);
+    var inputQtValue = parseInt(productQt.value);
+    if (inputNumValue != parseInt(target.innerHTML[0]) ) {
+        productNumber.value = target.innerHTML[0];
+        productQt.value = 1;
     }
     else {
-        inputvalue += 1;
-        productQt.setAttribute("value", inputvalue); 
-     
+        inputQtValue += 1;
+        productQt.value = inputQtValue;
+        quantity = inputQtValue;
     }
 }
-
